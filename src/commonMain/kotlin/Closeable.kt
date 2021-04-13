b9ab4566b77805ebbf4a45384a4d6490fd01d0d1
Rob Gulley <sirgulley@gmail.com>
2021-04-13 02:02:16 -0400

    common main base interface or expected declarations for peripheral manager, i2c, and gpio

---
+++ src/commonMain/kotlin/Closeable.kt
@@ -0,0 +1,3 @@
+interface Closeable {
+    fun close()
+}
---
+++ src/commonMain/kotlin/I2cDevice.kt
@@ -0,0 +1,16 @@
+interface I2cDevice : Closeable {
+    fun readRegByte(register: Int): Byte
+
+    fun readRegWord(register: Int): Int
+
+    fun writeRegByte(register: Int, data: Byte)
+    fun writeRegWord(register: Int, data: Short)
+
+    fun read(dataArray: ByteArray, address: Int)
+
+    fun write(data: ByteArray, address: Int)
+
+    fun readRegBuffer(register: Int, dataArray: ByteArray, length: Int)
+
+    fun writeRegBuffer(register: Int, dataArray: ByteArray, length: Int)
+}

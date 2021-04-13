80f0aacbe57fccb161a2b85dc4835f3e3bcacd6b
Rob Gulley <sirgulley@gmail.com>
2021-04-13 01:54:31 -0400

    turning off irrelevant native targets for now, leaving interop def config options to config file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("multiplatform") version "1.5.0-M2"
 }
@@ -10,7 +12,7 @@ repositories {
 }
 
 kotlin {
-    configure(listOf(linuxX64(), macosX64(), linuxArm64())) {
+    configure(listOf(linuxX64())) {
         binaries {
             executable {
                 entryPoint = "main"
@@ -18,19 +20,25 @@ kotlin {
         }
         val main by compilations.getting
         val mraa by main.cinterops.creating {
-            when (preset) {
-//                presets["macosX64"] -> includeDirs.headerFilterOnly("/usr/local/include")
-                presets["linuxX64"] -> includeDirs.headerFilterOnly("/usr/include", "/usr/include/x86_64-linux-gnu")
-            }
+//            when (preset) {
+////                presets["macosX64"] -> includeDirs.headerFilterOnly("/usr/local/include")
+//                presets["linuxX64"] -> includeDirs.headerFilterOnly("/usr/include", "/usr/include/x86_64-linux-gnu")
+//            }
         }
     }
 
     sourceSets {
-        val linuxArm64Main by getting
-        val linuxArm64Test by getting
+        val commonMain by getting{
+            dependencies{
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val commonTest by getting
+//        val linuxArm64Main by getting
+//        val linuxArm64Test by getting
         val linuxX64Main by getting
         val linuxX64Test by getting
-        val macosX64Main by getting
-        val macosX64Test by getting
+//        val macosX64Main by getting
+//        val macosX64Test by getting
     }
 }

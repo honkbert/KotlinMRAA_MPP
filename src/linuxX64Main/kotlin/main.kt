10d125334f8f92ed9cb104c72a49c15251e7dda0
Rob Gulley <sirgulley@gmail.com>
2021-04-13 01:55:49 -0400

    sample driver file for hardware tests

--- src/linuxX64Main/kotlin/main.kt
+++ src/linuxX64Main/kotlin/main.kt
@@ -1,4 +1,14 @@
+import kotlinx.cinterop.convert
+import platform.posix.sleep
+
 fun main() {
     println("Hello, Kotlin/Native!")
-
+    val peripheralManager = PeripheralManager()
+//    val device = peripheralManager.openI2cDevice("I2C1", 0x0)
+//    device.close()
+    val pin = peripheralManager.openGpio("BCM22")
+    pin.direction = GpioPin.Direction.OUT_START_LOW
+    pin.on()
+    sleep(1.convert())
+    pin.off()
 }

f57182a457231fc0b4dde78e9791b5ecae10f5b6
Rob Gulley <sirgulley@gmail.com>
2021-04-16 15:52:18 -0400

    added a basic blink test, added a basic test where one pin reads another pin's output

--- src/linuxX64Main/kotlin/main.kt
+++ src/linuxX64Main/kotlin/main.kt
@@ -1,14 +1,63 @@
+import com.robgulley.Sleep
 import kotlinx.cinterop.convert
 import platform.posix.sleep
 
-fun main() {
+fun main(args: Array<String>) {
     println("Hello, Kotlin/Native!")
+   basicRead(args[0].toInt(), args[1].toInt(), args[2].toInt())
+}
+private fun basicRead(sourcePin: Int, targetPin: Int, repeat:Int){
     val peripheralManager = PeripheralManager()
-//    val device = peripheralManager.openI2cDevice("I2C1", 0x0)
-//    device.close()
-    val pin = peripheralManager.openGpio("BCM22")
-    pin.direction = GpioPin.Direction.OUT_START_LOW
-    pin.on()
-    sleep(1.convert())
-    pin.off()
+    val pinSource = peripheralManager.openGpio(sourcePin)
+    Sleep.blockFor(100)
+    val pinTarget = peripheralManager.openGpio(targetPin)
+    Sleep.blockFor(100)
+    pinSource.direction = GpioPin.Companion.Direction.OUT
+    pinTarget.direction = GpioPin.Companion.Direction.IN
+    println("directions set")
+    for (count in 1..repeat) {
+        println("test $count of $repeat")
+        pinSource.off()
+        println("source off")
+        Sleep.blockFor(500)
+        val read1 = pinTarget.value
+        println("slept .5 second, target pin reads $read1")
+        pinSource.on()
+        println("source on")
+        Sleep.blockFor(500)
+        val read2 = pinTarget.value
+        println("slept .5 sceond, target pin reads $read2")
+        pinSource.off()
+        println("off")
+    }
+    pinSource.close()
+    pinTarget.close()
+    println("closed")
+}
+
+private fun basicLoop(pins: Array<Int>) {
+    val peripheralManager = PeripheralManager()
+    for (i in pins) {
+        try {
+            println("try pin $i")
+            val pin = peripheralManager.openGpio(i)
+            println("init'd")
+            pin.direction = GpioPin.Companion.Direction.OUT
+            println("out set")
+            pin.off()
+            println("off")
+            sleep(1.convert())
+            println("slept 1")
+            pin.on()
+            println("on")
+            sleep(1.convert())
+            println("slept 1")
+            pin.off()
+            println("off")
+            pin.close()
+            println("closed")
+        } catch (e: Throwable) {
+            println(e)
+        }
+    }
 }

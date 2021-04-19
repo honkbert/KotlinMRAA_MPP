1a430fb098758921b6f2e68b0755a025550d2113
Rob Gulley <sirgulley@gmail.com>
2021-04-19 01:00:41 -0400

    adding close method to i2c device

--- src/desktopMain/kotlin/com/robgulley/hwint/I2cDevice.kt
+++ src/desktopMain/kotlin/com/robgulley/hwint/I2cDevice.kt
@@ -2,17 +2,16 @@ package com.robgulley.hwint
 
 interface I2cDevice {
     fun readRegByte(register: Int): Byte
-
     fun readRegWord(register: Int): Int
 
     fun writeRegByte(register: Int, data: Byte)
     fun writeRegWord(register: Int, data: Short)
 
     fun read(dataArray: ByteArray, address: Int)
-
     fun write(data: ByteArray, address: Int)
 
     fun readRegBuffer(register: Int, dataArray: ByteArray, length: Int)
-
     fun writeRegBuffer(register: Int, dataArray: ByteArray, length: Int)
+
+    fun close()
 }

3af7237b2bfa2e39bee6e53c62f998d6fd8678e6
Rob Gulley <sirgulley@gmail.com>
2021-04-26 00:42:29 -0400

    removed general read and write methods from i2c device interface

--- src/desktopMain/kotlin/com/robgulley/hwint/I2cDevice.kt
+++ src/desktopMain/kotlin/com/robgulley/hwint/I2cDevice.kt
@@ -7,9 +7,6 @@ interface I2cDevice {
     fun writeRegByte(register: Int, data: Byte)
     fun writeRegWord(register: Int, data: Short)
 
-    fun read(dataArray: ByteArray, address: Int)
-    fun write(data: ByteArray, address: Int)
-
     fun readRegBuffer(register: Int, dataArray: ByteArray, length: Int)
     fun writeRegBuffer(register: Int, dataArray: ByteArray, length: Int)
 

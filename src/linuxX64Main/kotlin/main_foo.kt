b3fe1a2ce8aa84164759aeafdc995e00510bc655
Rob Gulley <sirgulley@gmail.com>
2021-05-05 20:47:08 -0400

    adding a test for reading the buffer from the UART device

--- src/linuxX64Main/kotlin/main_foo.kt
+++ src/linuxX64Main/kotlin/main_foo.kt
@@ -1,10 +1,8 @@
-import com.robgulley.Sleep
-import com.robgulley.hwint.GpioPin
-import com.robgulley.hwint.PeripheralManager
-import com.robgulley.hwint.format
+import com.robgulley.hwint.*
 import com.robgulley.hwint.test.MPU6050
-import com.robgulley.hwint.toHexString
+import com.robgulley.time.Sleep
 import kotlinx.cinterop.convert
+import kotlinx.cinterop.toKString
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
@@ -14,14 +12,35 @@ import platform.posix.sleep
 import kotlin.native.concurrent.freeze
 import kotlin.system.getTimeMicros
 
+@OptIn(ExperimentalUnsignedTypes::class)
 fun main_foo(args: Array<String>): Unit = runBlocking {
     println("Hello, Kotlin/Native!")
-
-    gpioInterruptTest(args[0].toInt(), args[1].toInt(), args[2].toInt(), this)
-    i2cReadWriteTest(this)
 }
 
-private suspend fun i2cReadWriteTest(coroutineScope: CoroutineScope) = coroutineScope.launch  {
+@OptIn(ExperimentalUnsignedTypes::class)
+private fun uartTest(uartName: String, coroutineScope: CoroutineScope) = coroutineScope.launch {
+    val CHUNK_SIZE = 512
+    val uartDevice = PeripheralManager().openUartDevice(uartName, this)
+    uartDevice.baudRate = 9600
+    uartDevice.registerUartDeviceCallback(object : UartDeviceCallback {
+        override fun onUartDeviceDataAvailable(uart: UartDevice): Boolean {
+            val buffer = ByteArray(CHUNK_SIZE)
+            var count: Int
+            while (uart.read(buffer, buffer.size.toLong()).also { count = it } > 0) {
+                println("${buffer.toKString()} --- $count")
+            }
+            return true
+        }
+
+
+        override fun onUartDeviceError(uart: UartDevice, error: Int) {
+            throw Exception("uart device error $error")
+        }
+
+    })
+}
+
+private suspend fun i2cReadWriteTest(coroutineScope: CoroutineScope) = coroutineScope.launch {
     val peripheralManager = PeripheralManager()
     val mpu6050 = MPU6050(peripheralManager, this)
     delay(500)
@@ -49,59 +68,60 @@ private fun i2cHelloTest() = runBlocking {
     printlnTime("who am I? expect: 0x73, actual ${whoAmIBuffer.toHexString()}")
 }
 
-private fun gpioInterruptTest(sourcePin: Int, targetPin: Int, repeat: Int, coroutineScope: CoroutineScope) = coroutineScope.launch {
-    val peripheralManager = PeripheralManager()
-    val pinSource = peripheralManager.openGpio(sourcePin)
-    delay(100)
-    val pinTarget = peripheralManager.openGpio(targetPin)
-    delay(100)
+private fun gpioInterruptTest(sourcePin: Int, targetPin: Int, repeat: Int, coroutineScope: CoroutineScope) =
+    coroutineScope.launch {
+        val peripheralManager = PeripheralManager()
+        val pinSource = peripheralManager.openGpio(sourcePin)
+        delay(100)
+        val pinTarget = peripheralManager.openGpio(targetPin)
+        delay(100)
 
-    pinSource.direction = GpioPin.Companion.Direction.OUT
-    pinTarget.direction = GpioPin.Companion.Direction.IN
-    pinTarget.edgeTriggerType = GpioPin.Companion.EdgeTriggerType.RISING
+        pinSource.direction = GpioPin.Companion.Direction.OUT
+        pinTarget.direction = GpioPin.Companion.Direction.IN
+        pinTarget.edgeTriggerType = GpioPin.Companion.EdgeTriggerType.RISING
 
 //    val flow = MutableSharedFlow<String>(replay = 4, extraBufferCapacity = 4).freeze()
-    val broadcastChannel = BroadcastChannel<String>(BUFFERED).freeze()
-    val receiveChannel = broadcastChannel.openSubscription().freeze()
+        val broadcastChannel = BroadcastChannel<String>(BUFFERED).freeze()
+        val receiveChannel = broadcastChannel.openSubscription().freeze()
 
-    launch {
+        launch {
 //        CoroutineWorker.withContext(IODispatcher) {
-        printlnTime("start")
-        val list = mutableListOf<String>()
-        printlnTime("start flow")
-        receiveChannel.consumeEach { list.add(it) }
+            printlnTime("start")
+            val list = mutableListOf<String>()
+            printlnTime("start flow")
+            receiveChannel.consumeEach { list.add(it) }
 //            flow.take(4).collect { list.add(it) }
-        printlnTime("got flow")
-        list.forEach { println(it) }
+            printlnTime("got flow")
+            list.forEach { println(it) }
 //        }
-    }
+        }
 
-    pinTarget.registerGpioCallback {
-        val time = (getTimeMicros() - started) / 1000000.0
-        broadcastChannel.offer("$time")
-    }
-    printlnTime("directions set")
+        pinTarget.registerGpioCallback {
+            val time = (getTimeMicros() - started) / 1000000.0
+            broadcastChannel.offer("$time")
+        }
+        printlnTime("directions set")
 
-    for (count in 1..repeat) {
-        printlnTime("test $count of $repeat")
-        pinSource.off()
-        printlnTime("source off")
-        delay(250)
-        printlnTime("slept .25 second")
-        pinSource.on()
-        printlnTime("source on")
-        delay(250)
-        printlnTime("slept .25 second")
-        pinSource.off()
-        printlnTime("off")
-    }
+        for (count in 1..repeat) {
+            printlnTime("test $count of $repeat")
+            pinSource.off()
+            printlnTime("source off")
+            delay(250)
+            printlnTime("slept .25 second")
+            pinSource.on()
+            printlnTime("source on")
+            delay(250)
+            printlnTime("slept .25 second")
+            pinSource.off()
+            printlnTime("off")
+        }
 
-    pinSource.close()
-    pinTarget.unregisterGpioCallback()
-    pinTarget.close()
-    printlnTime("closed")
-    broadcastChannel.close()
-}
+        pinSource.close()
+        pinTarget.unregisterGpioCallback()
+        pinTarget.close()
+        printlnTime("closed")
+        broadcastChannel.close()
+    }
 
 private fun basicGpioRead(sourcePin: Int, targetPin: Int, repeat: Int) = runBlocking {
     val peripheralManager = PeripheralManager()

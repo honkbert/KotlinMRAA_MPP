ec71079bb2ceae7ef163c9201fa789fd9511f32f
Rob Gulley <sirgulley@gmail.com>
2021-05-05 20:40:32 -0400

    adding dummy/todo files for Mac Os X target

--- src/macosX64Main/kotlin/com/robgulley/hwint/PeripheralManager.kt
+++ src/macosX64Main/kotlin/com/robgulley/hwint/PeripheralManager.kt
@@ -1,11 +1,20 @@
 package com.robgulley.hwint
 
+import kotlinx.coroutines.CoroutineScope
+
 actual class PeripheralManager {
     actual fun openI2cDevice(bus: String, address: Int): I2cDevice {
         TODO("Not yet implemented")
     }
 
     actual fun openGpio(pin: Int): GpioPin {
+        TODO("Not yet implemented")
+    }
+
+    actual fun openUartDevice(
+        uartName: String,
+        coroutineScope: CoroutineScope?
+    ): UartDevice {
         TODO("Not yet implemented")
     }
 }
